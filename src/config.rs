bfcac1d128afa63c6d8ea0ad175b14faadd92fae
Dmitrii K <dmitriik@protonmail.com>
2024-01-27 22:20:16 +0700

    add config

---
+++ src/config.rs
@@ -0,0 +1,18 @@
+use serde_derive::Deserialize;
+use std::{error::Error, fs};
+use toml;
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub mastodon_token: String,
+    pub mastodon_url: String,
+    pub max_post_len: i32,
+}
+
+impl Config {
+    pub fn new(config_file_path: &str) -> Result<Self, Box<dyn Error>> {
+        let file_content = fs::read_to_string(config_file_path)?;
+        let config: Config = toml::from_str(&file_content)?;
+        Ok(config)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+pub mod config;
+
+use clap::Parser;
+use log::error;
+
+use config::Config;
+
+#[derive(Parser, Debug)]
+struct CliArgs {
+    // path to .toml config file
+    #[arg(short, long)]
+    config: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let cli_args = CliArgs::parse();
+    let config = Config::new(&cli_args.config).unwrap_or_else(|err| {
+        error!("Can't parse cli arguments: {}", err);
+        std::process::exit(1);
+    });
+    println!("{:?}", config);
 }
